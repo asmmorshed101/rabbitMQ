df0394f17b0f2700543c3c0503d46056cc5055a2
asmmorshed101 <mm.morshed28707@gmail.com>
2025-07-08 13:10:12 +0900

    Commenting on Code

--- direct_consumer.py
+++ direct_consumer.py
@@ -1,24 +1,40 @@
-import pika
-import sys
+import pika # Python libray for RabbitMQ, we can communicate with RabbitMQ with this library
+import sys  # Take input from terminal
 
+# Creating conection with RabbitMQ server which is running in localhost. BlockingConnection --> sysnc connection - it will wait until finishing the message.
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+# From the connection creating a channel. All the task is done through this channel like making queue, sending message.
 channel = connection.channel()
 
+# Creating a channel name direct_logs and declared type direct. It means using routing key it will go to a specific queue.
 channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
 
+# Here it has been declared a random queue because name '' and exclusive = Ture means this queue will contain till the connection, if the connection close then queue will disappear. 
 result = channel.queue_declare('', exclusive=True)
+
+# Here it is contianing the queue name
 queue_name = result.method.queue
 
-# CLI থেকে severity গুলো নিই
+# Taking input from command line. If not given then default is info
+# Like python direct_consumer.py warning error
+# Here severities = ['warning', 'error']
+
 severities = sys.argv[1:] if len(sys.argv) > 1 else ['info']
 
+# Sending message to the specific queue after getting the key from severities. 
 for severity in severities:
     channel.queue_bind(exchange='direct_logs', queue=queue_name, routing_key=severity)
 
 print(f"🟢 Waiting for messages for severities: {severities}")
 
+# After getting the message what if will print the routing key and message 
 def callback(ch, method, properties, body):
     print(f"📥 [{method.routing_key}] {body.decode()}")
 
+# Take message from queue, process it through callback, auto_ack=True means consumer will acknowledge RabbitMQ after getting a message. 
+
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+# It will start listening message untill you press Ctrl+C
 channel.start_consuming()
--- direct_producer.py
+++ direct_producer.py
@@ -1,20 +1,30 @@
-import pika
-import sys
+import pika # Python libray for RabbitMQ, we can communicate with RabbitMQ with this library
+import sys  # Take input from terminal
 
+# Creating conection with RabbitMQ server which is running in localhost. BlockingConnection --> sysnc connection - it will wait until finishing the message. 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+# From the connection creating a channel. All the task is done through this channel like making queue, sending message.
 channel = connection.channel()
 
+# Creating a channel name direct_logs and declared type direct. It means using routing key it will go to a specific queue.
 channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
 
-# CLI থেকে severity (routing key) আর message নিতে চাই
+# It will take routing key from command line like info, warning, error. If not given then it will ifo by default
 severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
+# It will take message part if not given then default "Hello Direct Exchange!"
 message = ' '.join(sys.argv[2:]) if len(sys.argv) > 2 else 'Hello Direct Exchange!'
 
+# Example if you write python direct_producer.py error "Database connection failed"
+# Then serverity = 'error' and message = 'Database connection failed'
+
+# Here declareing message where it will go
 channel.basic_publish(
     exchange='direct_logs',
     routing_key=severity,
     body=message
 )
 
+# We can understand which routing key and what message has been sent
 print(f"✅ Sent '{message}' with severity '{severity}'")
 connection.close()
